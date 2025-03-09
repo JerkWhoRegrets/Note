e39b613c32aa5cc0b467cdbc49b8fbf84f9abb3d
JerkWhoRegrets <37231566+JerkWhoRegrets@users.noreply.github.com>
2025-03-09 23:30:26 +0800

    Delete src/demo.js

--- src/demo.js
+++
@@ -1,8 +0,0 @@
-function SimpleController($scope){
-    console.info("fdsf");
-    $scope.customers = [
-        {name: "Jone Doe",city: "New York"},
-        {name: "Jane Doe",city: "Beijing"},
-        {name: "Jack",city: "Shanghai"}
-    ];
-}
